e975233c579cb3e05cd4166893256f27965ee7fb
RoryEdmonds <123957532+BigRorzUni@users.noreply.github.com>
2026-02-17 00:59:57 +0000

    Added a testing script that generates performance logs

--- Assets/Scripts/Profiling/TestRunner.cs
+++ Assets/Scripts/Profiling/TestRunner.cs
@@ -1,8 +1,13 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.Profiling;
+using Unity.Profiling;
+using Unity.Profiling.LowLevel.Unsafe;
+using System.IO;
 
 public class TestRunner : MonoBehaviour
 {
@@ -13,6 +18,11 @@ public class TestRunner : MonoBehaviour
 
     private bool testFlags = false;
     private bool testing = false;
+
+    private string logDir = null;
+
+    private ProfilerRecorder cpuFrameRec;
+    private ProfilerRecorder gpuFrameRec;
 
     private void Awake()
     {
@@ -37,10 +47,10 @@ public class TestRunner : MonoBehaviour
                 case "-frames":
                     if(int.TryParse(args[i+1], out int frames))
                         framesToCapture = frames;
-                    continue;
-                
+                    break;
+
                 default:
-                    continue;
+                    break;
             }
         }
             
@@ -52,47 +62,134 @@ public class TestRunner : MonoBehaviour
 
         // uncap framerate
         Application.targetFrameRate = -1;
+
+        // initialise log dir path
+        string appPath = Application.dataPath;
+        string buildFolder = Directory.GetParent(appPath).Parent.Parent.FullName;
+
+        logDir = Path.Combine(buildFolder, "ProfilingLogs");
     }
 
     private IEnumerator RunAllTests()
     {
         foreach(string sceneName in testScenes)
         {
-            // profiling tag so i can see what test scene is being executed when using XCode for GPU data
-            UnityEngine.Profiling.Profiler.BeginSample($"******SCENE {sceneName}******");
-
             // run profiling here
             Debug.Log($"Finding Scene {sceneName}\n");
-            SceneManager.LoadScene(sceneName);
+
+            var op = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+            if (op == null)
+            {
+                Debug.LogError($" Could not load scene, is '{sceneName}' in build settings?");
+                yield break;
+            }
+
+            while (!op.isDone) yield return null; // wait until a scene is loaded before continuing
+
             Debug.Log($"Loaded scene: {sceneName}");
 
             // run startup frames - skip them
+            Debug.Log("Warmup frames...");
             for (int i = 0; i < startupFrames; i++)
                 yield return null;
 
             // run 500 frames
+            //Profiler.BeginSample($"******SCENE {sceneName}******"); // may use later
+
+            double[] cpuTimings = new double[framesToCapture];
+            double[] gpuTimings = new double[framesToCapture];
+
+            Debug.Log("Capturing frames...");
             for(int i = 0; i < framesToCapture; i++)
+            {   
                 yield return null;
+                
+                double cpuMs = cpuFrameRec.LastValue / 1_000_000.0;
+                double gpuMs = gpuFrameRec.LastValue / 1_000_000.0;
 
-                // measure these frames
+                cpuTimings[i] = cpuMs;
+                gpuTimings[i] = gpuMs;
 
-                // upload to csv
+                // store to array
+                //Debug.Log($"Frame {i}: CPU {cpuMs:F3} ms, GPU {gpuMs:F3} ms");
+            }
 
-            UnityEngine.Profiling.Profiler.EndSample();
+            string path = Path.Combine(logDir, $"{sceneName}.csv");
+
+            using StreamWriter sw = new StreamWriter(path, false);
+            sw.WriteLine("frame num, cpu frame (ms), gpu frame (ms)");
+            for(int i = 0; i < cpuTimings.Length; i++)
+            {
+                sw.Write(i.ToString());
+                sw.Write(",");
+                sw.Write(cpuTimings[i].ToString());
+                sw.Write(",");
+                sw.Write(gpuTimings[i].ToString());
+                sw.WriteLine();
+            }
+
+            Debug.Log($"Timings saved at {path}");
+
+
+            //Profiler.EndSample();
 
             // reload scene and run again for averaging??
         }
 
+        // send arrays to csv
+
+        Debug.Log("Testing done!");
         Application.Quit();
     }
+
 
     private void Start()
     {
         if(testing || !testFlags)
             return;
 
+        // output all profiling markers
+        // foreach (ProfilerCategory cat in Enum.GetValues(typeof(ProfilerCategory)))
+        // {
+        //     var handles = new System.Collections.Generic.List<ProfilerRecorderHandle>();
+        //     ProfilerRecorderHandle.GetAvailable(handles);
+        //     foreach (var h in handles)
+        //     {
+        //         var d = ProfilerRecorderHandle.GetDescription(h);
+        //         Debug.Log($"{d.Name}");
+        //     }
+        // }
+
+        // var handles = new List<ProfilerRecorderHandle>();
+        // ProfilerRecorderHandle.GetAvailable(handles);
+        // foreach (var h in handles)
+        // {
+        //     var name = ProfilerRecorderHandle.GetDescription(h).Name;
+        //     if (name.Contains("Frame") || name.Contains("GPU") || name.Contains("CPU"))
+        //         Debug.Log(name);
+        // }
+
         testing = true;
-        Debug.Log("Starting tests");
+        
+        Directory.CreateDirectory(logDir);
+        Debug.Log($"Starting tests. logDir = {logDir}, startupFrames = {startupFrames}, frames = {framesToCapture}");
+
+
+        cpuFrameRec = ProfilerRecorder.StartNew(ProfilerCategory.Internal, "CPU Total Frame Time");
+        gpuFrameRec = ProfilerRecorder.StartNew(ProfilerCategory.Internal, "GPU Frame Time");
+
+        if(!cpuFrameRec.Valid)
+        {
+            Debug.LogError("CPU recorder is not valid");
+            return;
+        }
+
+        if(!gpuFrameRec.Valid)
+        {
+            Debug.LogError("GPU recorder is not valid?");
+            return;
+        }
+
         StartCoroutine(RunAllTests());
     }
 }
